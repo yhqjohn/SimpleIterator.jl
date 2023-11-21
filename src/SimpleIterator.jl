5092476d878f63d5db0bb7288d36a6b474389e8a
yhqjohn <25428156+yhqjohn@users.noreply.github.com>
2023-11-21 20:18:15 +0800

    test passed

--- src/SimpleIterator.jl
+++ src/SimpleIterator.jl
@@ -1,5 +1,96 @@
 module SimpleIterator
 
-# Write your package code here.
+using MacroTools
 
+function iterator end
+IteratorType(::Any) = Any
+
+
+"""
+    @iterfn
+
+Define a iterator for a type. The type should have a function named `iterator` which returns a iterator.
+After the macro is called, iteration over the type will automatically becomes iteration over the iterator.
+
+# Arguments
+
+- `fundef`: function definition to be decorated, the function name should be `iterator`.
+
+# Example
+
+```julia
+struct MyType
+    data::Vector{Int}
 end
+
+@iterfn iterator(x::MyType) = x.data
+
+for i in MyType([1, 2, 3])
+    println(i)
+end
+```
+"""
+macro iterfn(fundef)
+    sig = MacroTools.splitdef(fundef)
+    args = sig[:args]
+    length(args) == 1 || error("Only one argument is allowed for iterator function")
+    arg = args[1]
+    # capture the type annotation
+    @capture(arg, (argname_::anno_)|(::anno_))||error("`iterate` function should have only one argument, with bounded type annotation")
+    bounds = sig[:whereparams]
+    name = get(sig, :name, nothing)
+    kwargs = get(sig, :kwargs, nothing)
+    rtype = get(sig, :rettype, nothing)
+
+    if name === nothing
+        throw(ArgumentError("Iterator function name is required"))
+    elseif name !== :iterator
+        throw(ArgumentError("Iterator function name must be `iterator`"))
+    end
+
+    if kwargs != []
+        throw(ArgumentError("Iterator function should not have keyword arguments"))
+    end
+
+    rst = quote
+        $fundef
+
+        function Base.iterate(x::$anno) where {$(bounds...)}
+            _iterator = iterator(x)
+            next = iterate(_iterator)
+            if next === nothing
+                return nothing
+            else
+                result, state = next
+                state = (_iterator, state)
+                return result, state
+            end
+        end
+
+        function Base.iterate(::$anno, state) where {$(bounds...)}
+            _iterator, state = state
+            next = iterate(_iterator, state)
+            if next === nothing
+                return nothing
+            else
+                result, state = next
+                state = (_iterator, state)
+                return result, state
+            end
+        end
+
+        Base.IteratorSize(::Type{$anno}) where {$(bounds...)} = Base.IteratorSize(IteratorType($anno))
+        Base.IteratorEltype(::Type{$anno}) where {$(bounds...)} = Base.IteratorEltype(IteratorType($anno))
+        Base.eltype(::Type{$anno}) where {$(bounds...)} = Base.eltype(IteratorType($anno))
+        Base.length(x::$anno) where {$(bounds...)} = Base.length(iterator(x))
+        Base.size(x::$anno) where {$(bounds...)} = Base.size(iterator(x))
+        Base.isdone(x::$anno) where {$(bounds...)} = Base.isdone(iterator(x))
+        Base.isdone(::$anno, state) where {$(bounds...)} = Base.isdone(state...)
+    end
+    return esc(rst)
+end
+
+export @iterfn, IteratorType, iterator
+
+end # module
+
--- test/runtests.jl
+++ test/runtests.jl
@@ -1,6 +1,105 @@
 using SimpleIterator
 using Test
 
-@testset "SimpleIterator.jl" begin
-    # Write your tests here.
+# Define a type to test the iterator macro
+struct MyType
+    data::Vector{Int}
+end
+
+# Define the iterator function for MyType
+@iterfn iterator(x::MyType) = x.data
+SimpleIterator.IteratorType(::Type{MyType}) = Vector{Int}
+
+# Define the tests
+@testset "Iterator Tests" begin
+    # Test iterating over MyType
+    @testset "Iteration" begin
+        mt = MyType([1, 2, 3])
+        expected = [1, 2, 3]
+        actual = [i for i in mt]
+        @test actual == expected
+    end
+
+    # Test IteratorSize
+    @testset "IteratorSize" begin
+        mt = MyType([1, 2, 3])
+        expected = Base.HasShape{1}()
+        actual = Base.IteratorSize(mt)
+        @test actual == expected
+    end
+
+    # Test IteratorEltype
+    @testset "IteratorEltype" begin
+        mt = MyType([1, 2, 3])
+        expected = Base.HasEltype()
+        actual = Base.IteratorEltype(mt)
+        @test actual == expected
+    end
+
+    # Test eltype
+    @testset "Eltype" begin
+        mt = MyType([1, 2, 3])
+        expected = Int
+        actual = Base.eltype(mt)
+        @test actual == expected
+    end
+
+    # Test length function
+    @testset "Length" begin
+        mt = MyType([1, 2, 3])
+        expected = 3
+        actual = length(mt)
+        @test actual == expected
+    end
+
+    # Test size function
+    @testset "Size" begin
+        mt = MyType([1, 2, 3])
+        expected = (3,)
+        actual = size(mt)
+        @test actual == expected
+    end
+
+    # Test isdone function
+    @testset "Is Done" begin
+        mt = MyType([1, 2, 3])
+        @test !isempty(mt)
+    end
+end
+
+# Test the iterator macro with different function definition styles
+@testset "Iterator Macro" begin
+    # Test the iterator macro with `function` style function definition
+    @testset "Equal Sign" begin
+        struct MyType2
+            data::Vector{Int}
+        end
+        @iterfn function iterator(x::MyType2)
+            return x.data
+        end
+        mt = MyType2([1, 2, 3])
+        @test [i for i in mt] == [1, 2, 3]
+    end
+
+    # Test the iterator macro with `function` style function definition with `where` clause
+    @testset "Equal Sign with Where Clause" begin
+        struct MyType3{T}
+            data::Vector{T}
+        end
+        @iterfn function iterator(x::MyType3{T}) where {T}
+            return x.data
+        end
+        mt = MyType3([1, 2, 3])
+        @test [i for i in mt] == [1, 2, 3]
+    end
+
+    # Test the iterator macro with `=` style function definition with `where` clause
+    @testset "Equal Sign with Where Clause" begin
+        struct MyType4{T}
+            data::Vector{T}
+        end
+        @iterfn (iterator(x::MyType4{T})) where {T} = x.data
+        mt = MyType4([1, 2, 3])
+        @test [i for i in mt] == [1, 2, 3]
+    end
 end
